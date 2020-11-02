bd3ab6fdad56a8f002edee2b51844237d1c468bb
tkaria <tkaria.info@gmail.com>
2020-11-02 16:14:33 +0900

    add uwp project

---
+++ EmbededUwp/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,67 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void Cube::appendToText(System.String)
+extern void Cube_appendToText_mF67FDA0A2B8D48DE8791DE8B2E4374019A566EDF (void);
+// 0x00000002 System.Void Cube::Update()
+extern void Cube_Update_mAE8B9D74FF9C49BB09EC76E0E30FEEB3B4DEEE8A (void);
+// 0x00000003 System.Void Cube::ChangeColor(System.String)
+extern void Cube_ChangeColor_mE9E5873C780B5E23E24A3F3F6AB339C867491FD0 (void);
+// 0x00000004 System.Void Cube::showHostMainWindow()
+extern void Cube_showHostMainWindow_m350B0DE5FF2EB8F6ECDB11DDD817112924FAECD3 (void);
+// 0x00000005 System.Void Cube::OnGUI()
+extern void Cube_OnGUI_mD15AC949E8393A4ADD9F2AA197C87D9869772124 (void);
+// 0x00000006 System.Void Cube::.ctor()
+extern void Cube__ctor_mCE6E9CAA9487D5900D8FEE93200CDCFF95AB5C9D (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	Cube_appendToText_mF67FDA0A2B8D48DE8791DE8B2E4374019A566EDF,
+	Cube_Update_mAE8B9D74FF9C49BB09EC76E0E30FEEB3B4DEEE8A,
+	Cube_ChangeColor_mE9E5873C780B5E23E24A3F3F6AB339C867491FD0,
+	Cube_showHostMainWindow_m350B0DE5FF2EB8F6ECDB11DDD817112924FAECD3,
+	Cube_OnGUI_mD15AC949E8393A4ADD9F2AA197C87D9869772124,
+	Cube__ctor_mCE6E9CAA9487D5900D8FEE93200CDCFF95AB5C9D,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	26,
+	23,
+	26,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
---
+++ EmbededUwp/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
@@ -0,0 +1,3 @@
+#include "il2cpp-config.h"
+#include "D:\workspace\Unity\uaal\EmbededUwp\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
+#include "D:\workspace\Unity\uaal\EmbededUwp\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"
